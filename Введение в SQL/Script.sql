d80ea2dd6192d4f8cda0773522d5b5e69ae93e7e
borovkovamv <67236753+borovkovamv@users.noreply.github.com>
2021-06-06 16:22:25 +0300

    Изменение скрипта (доработка ДЗ)

--- Введение в SQL/Script.sql
+++ Введение в SQL/Script.sql
@@ -1,7 +1,7 @@
-select table_name, 
-constraint_name 
-from information_schema.table_constraints 
-where constraint_schema = 'public' and constraint_type = 'PRIMARY KEY';
+select tc.table_name, kcu.column_name
+from information_schema.table_constraints tc
+join information_schema.key_column_usage kcu on tc.constraint_name = kcu.constraint_name 
+where tc.constraint_schema = 'public' and tc.constraint_type = 'PRIMARY KEY';
 
 SELECT * FROM country;
 
